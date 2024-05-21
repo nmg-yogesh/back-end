de06a7c6150c33d6842d31f2771c225551da95ff
yogesh dangi <dangi.yogesh93@gmail.com>
2024-05-21 19:07:14 +0530

    a test demo app

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express')
+
+const app = express()
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/test',(req,res)=>{
+    res.send("test page");
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
